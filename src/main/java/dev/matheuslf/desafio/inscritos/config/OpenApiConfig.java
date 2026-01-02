6697b3bdfdb109e7ef1d4a763ace9e0a0a52d52a
andreyrsy <contatomainort@gmail.com>
2026-01-02 10:07:59 -0300

    feat: swagger config

---
+++ src/main/java/dev/matheuslf/desafio/inscritos/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package dev.matheuslf.desafio.inscritos.config;
+
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                .title("Gerenciador de projetos")
+                .version("1.0")
+                .description("Desafio de um gerenciador projetos com tasks")
+                .contact(new Contact().name("").email("andreyrsy@gmail.com")))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+}
+
